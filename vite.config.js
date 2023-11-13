a80824854d54e3848f3218e8aa8f3e073ce2caca
Shubhang Shukla <shubhangshukla06@gmail.com>
2023-11-13 21:45:51 +0530

    change backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://stock-price-tracker-backend-one.vercel.app',
     },
   },
   plugins: [react()],
